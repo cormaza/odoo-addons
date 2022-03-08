6bef610a89a7a9198a15602e50a58d42f6c4303b
Christopher Ormaza <chris.ormaza@gmail.com>
2022-03-08 16:44:51 -0500

    [ADD] added active filed on campain record

--- utm_chatter/models/utm.py
+++ utm_chatter/models/utm.py
@@ -2,12 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-from odoo import models
+from odoo import fields, models
 
 
 class UtmCampaign(models.Model):
 
     _name = "utm.campaign"
+
+    active = fields.Boolean(string="Active?", default=True)
     _inherit = [
         "utm.campaign",
         "mail.thread",
