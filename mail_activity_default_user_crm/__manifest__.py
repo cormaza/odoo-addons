036e742bf0754ca3fa4bef74fb146d85a1d5a4ce
Christopher Ormaza <chris.ormaza@gmail.com>
2022-11-10 17:08:27 -0500

    [15.0][MIG] mail_activity_default_user_crm

--- mail_activity_default_user_crm/__manifest__.py
+++ mail_activity_default_user_crm/__manifest__.py
@@ -2,11 +2,10 @@
     "name": "Mail Activity Default User CRM",
     "summary": "Set commercial as default user on activities and calendar events",
     "author": "Christopher Ormaza",
+    "website": "https://github.com/cormaza/odoo-addons",
     "category": "Sales",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "depends": ["base", "calendar", "mail", "crm"],
-    "data": [
-        "views/assets.xml",
-    ],
+    "data": [],
     "license": "AGPL-3",
 }
