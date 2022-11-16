5494da58c44934fe904bb3ec11fd29c703021d60
Christopher Ormaza <chris.ormaza@gmail.com>
2022-11-16 13:36:59 -0500

    fix: action

--- hr_scheduled_transaction/tests/test_scheduled_transaction.py
+++ hr_scheduled_transaction/tests/test_scheduled_transaction.py
@@ -7,18 +7,21 @@ class ScheduledTransactionTest(TransactionCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         input_type_model = cls.env["hr.payslip.input.type"]
+        category_model = cls.env["hr.salary.rule.category"]
         employee_model = cls.env["hr.employee"]
         contract_model = cls.env["hr.contract"]
         cls.input_type_id = input_type_model.create(
             {
                 "name": "Extra Input",
                 "code": "ExtraInput",
+                "category_id": category_model.search([], limit=1).id,
             }
         )
         cls.output_type_id = input_type_model.create(
             {
                 "name": "Extra Output",
                 "code": "ExtraOutput",
+                "category_id": category_model.search([], limit=1).id,
             }
         )
         cls.employee_id = employee_model.create(
