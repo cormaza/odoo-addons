2d65772450a41e049f5d866e24116e77aa4352d1
Christopher Ormaza <chris.ormaza@gmail.com>
2023-03-30 16:55:24 -0500

    [15.0][ADD] website_sale_hide_price_contactus (#12)

---
+++ website_sale_hide_price_contactus/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Link to Contact us on Hide Price",
+    "summary": "Link to Contact us on Hide Price",
+    "author": "Christopher Ormaza",
+    "website": "https://github.com/cormaza/odoo-addons",
+    "category": "Sales",
+    "version": "15.0.1.0.0",
+    "depends": ["website_sale_hide_price", "website_crm"],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    "license": "AGPL-3",
+}
