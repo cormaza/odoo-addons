46a09a90320f3d5beb4fa55f85d4983de36c5d61
Christopher Ormaza <chris.ormaza@gmail.com>
2026-06-17 05:00:20 -0500

    Migration setup changes for Odoo 19

--- account_picking_anglo_saxon_sync/__manifest__.py
+++ account_picking_anglo_saxon_sync/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Invoice Picking Anglo Saxon Sync",
-    "version": "16.0.1.0.0",
+    "version": "19.0.1.0.0",
     "summary": "Invoice Picking Anglo Saxon Sync",
     "category": "Accounting",
     "author": "Christopher Ormaza",
